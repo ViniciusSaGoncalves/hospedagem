f2c7d59624a867c85aa927d0ac5c1f1e21d53f34
vinicius <vinicius.goncalvestj@gmail.com>
2021-05-06 08:41:40 -0400

    commit inicial

---
+++ src/test/java/com/vinicius/hospedagem/HospedagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.hospedagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
